e0f613061ae11fa182aea24cf98e16c3502376a3
xarlys <xarlysouza@hotmail.com>
2021-05-04 15:15:20 -0300

    Database docker

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,11 +1,10 @@
 import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
-export default async (host = "database_ignite"): Promise<Connection> => {
+export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
   return createConnection(
     Object.assign(defaultOptions, {
-      host: process.env.NODE_ENV === "test" ? "localhost" : host,
       database:
         process.env.NODE_ENV === "test"
           ? "rentx_test"
@@ -13,3 +12,20 @@ export default async (host = "database_ignite"): Promise<Connection> => {
     })
   );
 };
+
+
+// import { Connection, createConnection, getConnectionOptions } from "typeorm";
+
+// export default async (host = "database_ignite"): Promise<Connection> => {
+//   const defaultOptions = await getConnectionOptions();
+
+//   return createConnection(
+//     Object.assign(defaultOptions, {
+//       host: process.env.NODE_ENV === "test" ? "localhost" : host,
+//       database:
+//         process.env.NODE_ENV === "test"
+//           ? "rentx_test"
+//           : defaultOptions.database,
+//     })
+//   );
+// };
